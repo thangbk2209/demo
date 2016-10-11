f5ba794878d55bdee807eaa68450ba227864a47a
thangbk2209 <thangbk2209@gmail.com>
2016-10-11 03:03:02 +0000

    change port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello !');
 });
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
